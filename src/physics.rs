43ecf1a155eca9fb45de89194e53d6a30b4b1a1d
BD103 <59022059+BD103@users.noreply.github.com>
2024-04-20 17:53:38 -0400

    fix: use delta in physics

--- src/physics.rs
+++ src/physics.rs
@@ -7,14 +7,14 @@ pub struct Velocity(pub Vec2);
 #[derive(Component, Deref, DerefMut, Default)]
 pub struct Acceleration(pub Vec2);
 
-pub fn update_position(mut query: Query<(&mut Transform, &Velocity)>) {
+pub fn update_position(mut query: Query<(&mut Transform, &Velocity)>, time: Res<Time>) {
     for (mut transform, velocity) in &mut query {
-        transform.translation += math::compose_vec3(**velocity, 0.0);
+        transform.translation += math::compose_vec3(**velocity, 0.0) * time.delta_seconds();
     }
 }
 
-pub fn update_velocity(mut query: Query<(&mut Velocity, &Acceleration)>) {
+pub fn update_velocity(mut query: Query<(&mut Velocity, &Acceleration)>, time: Res<Time>) {
     for (mut velocity, acceleration) in &mut query {
-        **velocity += **acceleration;
+        **velocity += **acceleration * time.delta_seconds();
     }
 }
