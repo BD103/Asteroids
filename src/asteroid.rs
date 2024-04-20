b03c7d811523da531c3968c7d5a78c958855967f
BD103 <59022059+BD103@users.noreply.github.com>
2024-04-20 17:31:38 -0400

    feat: asteroid and ship drawing

--- src/asteroid.rs
+++ src/asteroid.rs
@@ -1,4 +1,23 @@
 use bevy::prelude::*;
 
+use crate::physics;
+
 #[derive(Component, Default)]
 pub struct Asteroid;
+
+pub fn spawn_asteroids(mut commands: Commands, input: Res<ButtonInput<KeyCode>>) {
+    if input.just_pressed(KeyCode::Space) {
+        commands.spawn((
+            Asteroid,
+            Transform::default(),
+            physics::Velocity(Vec2::ONE),
+        ));
+    }
+}
+
+pub fn draw_asteroids(query: Query<&Transform, With<Asteroid>>, mut gizmos: Gizmos) {
+    for transform in &query {
+        let position = Vec2::new(transform.translation.x, transform.translation.y);
+        gizmos.circle_2d(position, 8.0, Color::WHITE);
+    }
+}
--- src/ship.rs
+++ src/ship.rs
@@ -2,3 +2,21 @@ use bevy::prelude::*;
 
 #[derive(Component, Default)]
 pub struct Ship;
+
+pub fn spawn_ship(mut commands: Commands) {
+    commands.spawn((
+        Ship,
+        Transform::default(),
+    ));
+}
+
+pub fn draw_ship(query: Query<&Transform, With<Ship>>, mut gizmos: Gizmos) {
+    for transform in &query {
+        let start = Vec2::new(transform.translation.x, transform.translation.y);
+
+        let end = transform.translation + transform.local_x() * 10.0;
+        let end = Vec2::new(end.x, end.y);
+
+        gizmos.arrow_2d(start, end, Color::WHITE);
+    }
+}
